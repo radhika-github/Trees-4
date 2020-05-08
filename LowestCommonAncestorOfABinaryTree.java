e541e12ea6850ebd11b9cd1448ead780d0fdfa57
Radhika <radhi.virgo.16@gmail.com>
2020-05-07 20:15:58 -0500

    Solution

---
+++ LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// Time COmplexity: O(n)
+// Space COmplexity with recursive stack : O(h)
+// Space COmplexity without recursive stack : O(1)
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) {
+            return null;
+        }
+
+        if(root.val == p.val || root.val == q.val) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left == null)
+            return right;
+        if(right == null)
+            return left;
+        return root;
+    }
+}
